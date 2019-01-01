5b8c025e46d8470f9541f53f38994be006d221ee
Junaid TT <junaidkodoor@gmail.com>
2019-01-02 01:15:16 +0400

    templates and static folder added

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+posts = [
+	{
+		'author':'CoryMS',
+		'title' :'Blog Post 1',
+		'content':'First Post content',
+		'date_posted':'August 27, 2018'
+	},
+	{
+		'author':'Jane Doe',
+		'title' :'Blog Post 2',
+		'content':'Second Post content',
+		'date_posted':'August 28, 2018'
+	},
+]
+
 def home(request):
-	return HttpResponse('<h1>Blog Home</h1>')
+	context = {
+		'posts':posts
+	}
+	return render(request,'blog/home.html',context)
 
 def about(request):
-	return HttpResponse('<h1>About Us</h1>')
+	return render(request,'blog/about.html',{'title':'About'})
