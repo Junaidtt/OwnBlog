e13fd38ac123354a625f8cdc0f81364624afa968
Junaid TT <junaidkodoor@gmail.com>
2019-01-05 22:50:49 +0400

    User Registration

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from .models import Post
 # Create your views here.
-
-posts = [
-	{
-		'author':'CoryMS',
-		'title' :'Blog Post 1',
-		'content':'First Post content',
-		'date_posted':'August 27, 2018'
-	},
-	{
-		'author':'Jane Doe',
-		'title' :'Blog Post 2',
-		'content':'Second Post content',
-		'date_posted':'August 28, 2018'
-	},
-]
 
 def home(request):
 	context = {
-		'posts':posts
+		'posts':Post.objects.all()
 	}
 	return render(request,'blog/home.html',context)
 
