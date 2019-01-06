224918bdcc91ca64ff0e190baf02a797b74a0051
Junaid TT <junaidkodoor@gmail.com>
2019-01-06 23:15:52 +0400

    user profile created

--- django_project/settings.py
+++ django_project/settings.py
@@ -117,13 +117,18 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaSdefault.jpgcript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_URL = 'login'
 
 LOGIN_REDIRECT_URL = 'blog_home'
+
+
 
